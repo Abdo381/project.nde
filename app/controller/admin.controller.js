0b5c8f7248f866f4473bcb2376e3d463de6b6ac8
Abdo381 <71750074+Abdo381@users.noreply.github.com>
2022-03-05 23:51:52 +0200

    Update admin.controller.js

--- app/controller/admin.controller.js
+++ app/controller/admin.controller.js
@@ -1,11 +1,14 @@
-const userModel =require("../../database/models/user.model")
+const adminModel = require("../../database/models/admin.model")
+const otpGen = require("../helper/otp-gen")
 class admin{
     static register= async(req,res)=>{
         try{
-            const admin = new userModel(req.body)
-            await admin.save()
+            const admin = new adminModel(req.body)
+            await admin.save() 
+            const otp= otpGen(8)
+           
             res.send({
-                apiStatus:true, data: admin, message:"data added successfuly"
+                apiStatus:true, data: {admin, otp}, message:"data added successfuly"
             })
         }
         catch(e){
@@ -13,29 +16,74 @@ class admin{
         }
     }
     static login = async(req,res)=>{
-        try {
-            const admin = await adminModel.login(req.body.email,req.body.password)
+        try{
+            const admin = await adminModel.login(req.body.email, req.body.password)
             const token = await admin.generateToken()
             res.send({
                 apiStatus:true,
-                data:{admin,token},
+                data:{ admin, token }, 
                 message:"logged in"
             })
-
-        } catch (e) {
-            res.send({apiStatus:false,data:e.message , message:"invalid login"})
-
+        }
+        catch(e){
+            res.send({apiStatus:false, data:e.message, message:"invalid login"})
         }
     }
-    static logout= async(req,res)=>{
-        try {
-            req.admin.token = req.admin.token.filter (t => t.token !=req.token)
+    static me = async(req,res)=>{
+        res.send({apiStatus:true,data:req.admin, message:'data featched'})
+    }
+    static logout = async(req,res)=>{
+        try{
+            req.admin.tokens = req.admin.tokens.filter( t => t.token != req.token )
             await req.admin.save()
-            res.send({apiStatus:true ,data:{}, message:"logged out"})
-        } catch (e) {
+            res.send({apiStatus:true, data:{}, message:"logged out"})
+        }
+        catch(e){
             res.send({apiStatus:false, data:e.message, message:"error in logout"})
-
+        }
+    }
+    static logoutAll = async(req,res)=>{
+        try{
+            req.admin.tokens = []
+            await req.admin.save()
+            res.send({apiStatus:true, data:{}, message:"logged out"})
+        }
+        catch(e){
+            res.send({apiStatus:false, data:e.message, message:"error in logout"})
+        }
+    }
+    static getSingle = async(req,res)=>{
+        try{
+            const admin = await adminModel.findById(req.params.id) //statics
+            res.send({
+                apiStatus:true, data: admin, message:"data featched successfuly"
+            })
+        }
+        catch(e){
+            res.send({apiStatus:false, data:e.message, message:"error fetching admin"})
+        }
+    }
+    static delSingle = async(req,res)=>{
+        try{
+            const admin = await adminModel.findByIdAndDelete(req.params.id)
+            res.send({
+                apiStatus:true, data: admin, message:"data deleted successfuly"
+            })
+        }
+        catch(e){
+            res.send({apiStatus:false, data:e.message, message:"error deleting admin"})
+        }
+    }
+    static profileImg = async (req, res) =>{
+        try{
+           
+            req.admin.image = req.file.path
+            await req.admin.save()
+            res.send({apiStatus:true, data:req.admin, message:"image uploaded"})
+        }
+        catch(e){
+            res.send({apiStatus:false, data:e.message, message:"error deleting admin"})
         }
     }
 }
-module.exports =admin
+module.exports = admin
