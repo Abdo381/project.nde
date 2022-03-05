f0289de2801f8ce29f2df9a826ffbcf2da1bc436
Abdo381 <71750074+Abdo381@users.noreply.github.com>
2022-03-05 23:52:25 +0200

    Update category.controler.js

--- app/controller/category.controler.js
+++ app/controller/category.controler.js
@@ -1,49 +1,48 @@
 const categoryModel = require("../../database/models/category.model")
 class category{
-    static addCategory = async(req ,res)=>{
+    static addcategory= async(req,res)=>{
         try{
             const category = new categoryModel({
-               adminId:req.admin._id,
+                adminId:req.admin._id,
                 ...req.body
             })
             await category.save()
-            res.send({apiStatus:true, data:post, message:"added"})            
+            res.send({apiStatus:true, data:category, message:"added"})            
+        }
+        catch(e){
+            res.send({apiStatus:false, data:e.message, message:"errorAdd"})
+        }
+    }
+    static allcategorys= async(req,res)=>{
+        try{
+            const categorys = await categoryModel.find()
+            res.send({apiStatus:true, data:categorys, message:"data featched"})
         }
         catch(e){
             res.send({apiStatus:false, data:e.message, message:"error"})
         }
     }
-    static delCategory = async(req ,res)=>{
+    static mycategorys= async(req,res)=>{
+        try{
+            await req.admin.populate("mycategorys")
+            res.send({apiStatus:true, data:{admin:req.admin, categorys:req.admin.mycategorys}, message:"data featched"})
+        }
+        catch(e){
+            res.send({apiStatus:false, data:e.message, message:"error"})
+        }
+    }
+    static delcategory= async(req,res)=>{
         try{
             const categorys = await categoryModel.deleteOne({
                 _id:req.params.id,
                 adminId:req.admin._id
             })
-            if(!categorys) throw new Error("invalid post")
+            if(!categorys) throw new Error("invalid category")
             res.send({apiStatus:true, data:categorys, message:"data featched"})
         }
         catch(e){
             res.send({apiStatus:false, data:e.message, message:"error"})
-        }   
+        }    
     }
-    static allCategory = async(req ,res)=>{
-        try{
-            const category = await categoryModel.find()
-            res.send({apiStatus:true, data:category, message:"data featched"})
-        }
-        catch(e){
-            res.send({apiStatus:false, data:e.message, message:"error"})
-        }
-    }
-    static myCategory = async(req ,res)=>{
-        try{
-            await req.admin.populate("myCategory")
-            res.send({apiStatus:true, data:{admin:req.admin, posts:req.admin.myCategory}, message:"data featched"})
-        }
-        catch(e){
-            res.send({apiStatus:false, data:e.message, message:"error"})
-        }
-    }
-
 }
-module.exports=category
+module.exports = category
